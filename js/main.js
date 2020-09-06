4861043b0ddd9495ab42a31f1bd0e31d407069d6
Burkhanov Ildar <burhanoff1992@gmail.com>
2020-09-06 20:45:15 +0300

    dsadsad

--- js/main.js
+++ js/main.js
@@ -1,17 +1,39 @@
 (function ($) {
 
-    $(document).ready(function() {
-        const tableSlider = new Swiper('.table-slider', {
-            slidesPerView: 1,
-    
-            on: {
-                init: function () {
-                    console.log('ok');
-                }
-            }
-        });
+  $(document).ready(function() {
+    var body = $('body'),
+        burger = $('.header-burger'),
+        search = $('.header-search')
 
-        // продолжаем писать JS туть
+    burger.click(function () {
+      body.toggleClass('state-nav')
+    })
+
+
+    const tableSlider = new Swiper('.table-slider', {
+      slidesPerView: 1,
+  
+      on: {
+        init: function () {
+          //console.log('ok');
+        }
+      },
+      
+      pagination: {
+        el: '.table-nav-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="table-nav-item '+ className +'">' + (index + 1) + '</span>';
+        }
+      },
+
+      navigation: {
+        prevEl: '.table-nav-prev',
+        nextEl: '.table-nav-next'
+      }
     });
+
+    // продолжаем писать JS туть
+  });
 
 })(jQuery)
